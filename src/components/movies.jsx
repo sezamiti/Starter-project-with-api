83b9f31dfcf2ac310a1394ddbf3da2dadfc27631
Sezam <dossumbekov@list.ru>
2022-07-28 06:37:06 +0600

    Starter project with api

---
+++ src/components/movies.jsx
@@ -0,0 +1,18 @@
+import { Movie } from "./movie";
+
+function Movies(props) {
+    const { movies = [] } = props;
+
+    return (
+        <div className='movies'>
+            {movies.length ? (
+                movies.map((movie) => <Movie key={movie.imdbID} {...movie} />)
+            ) : (
+                <h4>Nothing found</h4>
+            )}
+        </div>
+    );
+}
+
+
+export {Movies} 
